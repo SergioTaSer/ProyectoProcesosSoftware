3272979b835d6847521908d417a9e519dd4ef172
Sergio Tarraga <staserj02@gmail.com>
2024-03-16 22:57:07 +0100

    Implementación inicial del Backend

---
+++ servidor/modeloSpec.js
@@ -0,0 +1,47 @@
+const modelo = require('./modelo.js');
+
+/** PRUEBAS GESTIÓN DE USUARIOS **/
+
+describe('El sistema...', function() {
+
+  let sistema;
+
+  //Bloque de inicialización de las pruebas, se ejecuta antes de cada una de las pruebas
+  beforeEach(function() {
+    sistema=new modelo.Sistema()
+  });
+
+  //Prueba 1: Verificar que inicialmente no hay usuarios
+  it('inicialmente no hay usuarios', function() {
+    expect(sistema.numeroUsuarios()).toEqual(0);
+  });
+
+  //Prueba 2: Agregar un usuario al sistema
+  it('agregar un usuario al sistema', function() {
+    sistema.agregarUsuario('user1');
+    expect(sistema.numeroUsuarios()).toEqual(1);
+  });
+
+  //Prueba 3: Obtener todos los usuarios del sistema
+  it('obtener todos los usuarios del sistema', function() {
+    sistema.agregarUsuario('user1');
+    sistema.agregarUsuario('user2');
+    expect(sistema.numeroUsuarios()).toEqual(2);
+  });
+
+  //Prueba 4: Verificar si un usuario está activo
+  it('verificar si un usuario está activo', function() {
+    sistema.agregarUsuario('user1');
+    expect(sistema.usuarioActivo('user1')).toEqual(true);
+  });
+  
+  //Prueba 5: Eliminar un usuario del sistema
+  it('eliminar un usuario del sistema', function() {
+    sistema.agregarUsuario('user1');
+    expect(sistema.numeroUsuarios()).toEqual(1);
+    sistema.eliminarUsuario('user1');
+    expect(sistema.numeroUsuarios()).toEqual(0);
+  });
+
+})
+  
